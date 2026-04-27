cd8cf01d2aa6b36ebe6a29834b677b009ca6c856
NYGS <n00199817@upn.pe>
2026-04-27 18:26:00 -0500

    inicio

---
+++ src/hospital/Hospital.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospital;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author UCC20417
+ */
+public class Hospital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+    }
+    
+}
